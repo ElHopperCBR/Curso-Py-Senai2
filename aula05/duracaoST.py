a87d1ad7093ae443939a2ac276c3b0b4cd0394e5
ElHopperCBR <cesar.brandao@sp.senai.br>
2025-11-05 18:21:06 -0300

    hoje

---
+++ aula05/duracaoST.py
@@ -0,0 +1,17 @@
+import streamlit as st 
+#Problema duração de tempo
+TITULO ="Calculadora de Duração de Tempo"
+st.set_page_config(page_title=TITULO)
+st.title(TITULO)
+
+#Entrada de dados
+tempo = st.number_input("Digite o tempo em segundos:",min_value=0, step=1,help="Insira a duração total em segundos para converter em horas, minutos e segundos.")
+
+
+
+#Processamento de dados
+horas = tempo // 3600 #Calculo das horas
+minutos = (tempo % 3600) // 60 #Calculo dos minutos
+segundos = tempo % 60 #Calculo dos segundos
+#Saída de dados
+st.write(f"{horas} horas, {minutos} minutos e {segundos} segundos.")
